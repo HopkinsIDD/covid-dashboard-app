1a9f37b214072ec053bc96dd23b3c624b34d8f9f
Lily Xu <lilyxu@alum.mit.edu>
2020-04-21 11:21:24 -0700

    Fake data for development will live in constants

---
+++ src/store/constants.js
@@ -0,0 +1,32 @@
+export const SCENARIOS = [
+    {'id': 1, 'name': 'Fixed Lockdown'},
+    {'id': 2, 'name': 'Fatiguing Lockdown'},
+    {'id': 3, 'name': 'Pulsed Lockdown'},
+    {'id': 4, 'name': 'Fixed, Pulsed Lockdown'},
+    // {'id': 1, 'name': 'Fixed Lockdown + Mild Social Distancing'},
+    // {'id': 2, 'name': 'Fatiguing Lockdown + Mild Social Distancing'},
+    // {'id': 3, 'name': 'Fixed Lockdown + Pulsed Social Distancing'},
+    // {'id': 4, 'name': 'Fatiguing Lockdown + Pulsed Social Distancing'},
+];
+
+export const STATS = [
+    {'id': 0, 'name': 'Infections'},
+    {'id': 1, 'name': 'Hospitalizations'},
+    {'id': 2, 'name': 'ICU Cases'},
+    {'id': 3, 'name': 'Ventilator Usage'},
+    {'id': 4, 'name': 'Deaths'},
+];
+
+export const COUNTIES = [
+    {'GEOID': '06067', 'NAME': 'Sacramento County', 'USPS': 'CA'},
+    {'GEOID': '06081', 'NAME': 'San Mateo County', 'USPS': 'CA'},
+    {'GEOID': '06085', 'NAME': 'Santa Clara County', 'USPS': 'CA'},
+    {'GEOID': '06071', 'NAME': 'San Bernardino County', 'USPS': 'CA'},
+    {'GEOID': '06073', 'NAME': 'San Diego County', 'USPS': 'CA'},
+    {'GEOID': '10001', 'NAME': 'Lee County', 'USPS': 'AL'},
+    {'GEOID': '10201', 'NAME': 'Tuskaloosa County', 'USPS': 'AL'},    
+    {'GEOID': '10301', 'NAME': 'DeKalb County', 'USPS': 'AL'},
+    {'GEOID': '10401', 'NAME': 'Jefferson County', 'USPS': 'AL'},
+    {'GEOID': '10501', 'NAME': 'Clay County', 'USPS': 'AL'},
+    {'GEOID': '10601', 'NAME': 'Coffee County', 'USPS': 'AL'},
+];
