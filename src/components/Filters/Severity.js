21bae1ed3ffc0016add87ec479269d06c029e6ce
Lily Xu <lilyxu@alum.mit.edu>
2020-04-24 10:02:59 -0700

    Fix to allow only 1 Severity button to be clicked

--- src/components/Filters/Severity.js
+++ src/components/Filters/Severity.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { LEVELS } from '../../store/constants.js';
 
 class Severity extends Component {
-    // TODO: only allow to click on one severity checked 
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
     }
     
     handleChange(i) {
-        console.log('inside severity', i)
         this.props.onSeverityClick(i.name);
     }
 
@@ -18,25 +16,25 @@ class Severity extends Component {
             <div>
                 <div className="form-check">
                     <input
-                        className="form-check-input" type="radio" value="option_1" id="level_1"
-                        onChange={() => this.handleChange(LEVELS[0])} checked/>
-                    <label className="form-check-label filter-text" htmlFor="level_1">
+                        className="form-check-input" type="radio" name="sev"
+                        onChange={() => this.handleChange(LEVELS[0])} />
+                    <label className="form-check-label filter-text">
                     {LEVELS[0].name}
                     </label>
                 </div>
                 <div className="form-check">
                     <input
-                        className="form-check-input" type="radio" value="option_2" id="level_2"
+                        className="form-check-input" type="radio" name="sev"
                         onChange={() => this.handleChange(LEVELS[1])} />
-                    <label className="form-check-label filter-text" htmlFor="level_2">
+                    <label className="form-check-label filter-text">
                     {LEVELS[1].name}
                     </label>
                 </div>                    
                 <div className="form-check">
                     <input
-                        className="form-check-input" type="radio" value="option_3" id="level_3"
+                        className="form-check-input" type="radio" name="sev"
                         onChange={() => this.handleChange(LEVELS[2])} />
-                    <label className="form-check-label filter-text" htmlFor="level_3">
+                    <label className="form-check-label filter-text">
                     {LEVELS[2].name}
                     </label>
                 </div>
