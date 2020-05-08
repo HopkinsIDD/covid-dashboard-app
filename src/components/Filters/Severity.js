98bc1b32d5c46a8ee45259e35ee84375726594fd
Lily Xu <lilyxu@alum.mit.edu>
2020-05-08 09:48:56 -0700

    Make deep copy of levels to avoid editing state

--- src/components/Filters/Severity.js
+++ src/components/Filters/Severity.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import { LEVELS } from '../../utils/constants.js';
 
 class Severity extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            levels: Array.from(LEVELS)
+        }
+    }
+
     handleChange = (item) => {
-        item.scenario = this.props.scenario.key; 
+        // item.scenario = this.props.scenario.key; 
         console.log('in severity', item)
         this.props.onSeverityClick(item);
     }
 
     render() {
         const { severity } = this.props;
+        console.log('sev render', this.state.levels)
         return (
             <div>
-                {LEVELS.map(level => {
+                {this.state.levels.map(level => {
                     /* console.log(this.props.scenario, level.key, level.key===severity.key) */
-                    const isActive = (level.key === severity.key) ? 'checked' : '';
+                    /* const isActive = (level.key === severity.key) ? 'checked' : ''; */
                     return (
                         <div className="form-check" key={level.id}>
                             <input
@@ -22,7 +30,9 @@ class Severity extends Component {
                                 type="radio"
                                 name="sev"
                                 onChange={() => this.handleChange(level)} 
-                                checked={isActive}/>
+                                // checked={isActive}
+
+                                />
                             <label className="form-check-label filter-label">
                             {level.name}
                             </label>
