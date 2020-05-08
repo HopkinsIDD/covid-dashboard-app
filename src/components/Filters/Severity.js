88ca154f731ed157c57961188acd49183196575b
Lily Xu <lilyxu@alum.mit.edu>
2020-05-08 15:20:57 -0700

    Separate radio groups and designate active element

--- src/components/Filters/Severity.js
+++ src/components/Filters/Severity.js
@@ -3,51 +3,29 @@ import _ from 'lodash';
 import { LEVELS } from '../../utils/constants.js';
 
 class Severity extends Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         levels: Array.from(LEVELS),
-    //     }
-    // }
-
-    componentDidMount() {
-        console.log('severity componentDidMount LEVELS', LEVELS)
-    }
-
     handleChange = (item) => {
-        // add scenario to severity obj so MainContainer knows which scenario is activated
-        // console.log('severityContainer this.state.levels',this.state.levels)
-        console.log('LEVELS', LEVELS) 
-
+        // add scenario to obj so MainContainer knows which scenario is active
         const itemClone = _.assign({}, item, {
             scenario: this.props.scenario.key
         });
-        // const itemCopy = _.cloneDeep(item);
-
-        // itemCopy.scenario = this.props.scenario.key; // i think this line is the problem
-
-        console.log('in severity', itemClone)
         this.props.onSeverityClick(itemClone);
     }
 
     render() {
-        // const { severity } = this.props;
-        // console.log('sev render', this.state.levels)
-        const levelsCopy = _.cloneDeep(LEVELS);
+        const { severity, scenario } = this.props;
         return ( 
             <div>
-                {levelsCopy.map(level => {
-                    /* console.log(this.props.scenario, level.key, level.key===severity.key) */
-                    /* const isActive = (level.key === severity.key) ? 'checked' : ''; */
+                {LEVELS.map(level => {
+                    const isActive = (severity.key === level.key
+                        && severity.scenario === scenario.key) ? 'checked' : '';
                     return (
                         <div className="form-check" key={level.id}>
                             <input
                                 className="form-check-input"
                                 type="radio"
-                                name="sev"
-                                onChange={() => this.handleChange(_.cloneDeep(level))} 
-                                // checked={isActive}
-
+                                name={`sev-${scenario.key}`}
+                                onChange={() => this.handleChange(level)} 
+                                checked={isActive}
                                 />
                             <label className="form-check-label filter-label">
                             {level.name}
