b802cfc6141ee38078852bc76b013454d682e9ce
Lily Xu <lilyxu@alum.mit.edu>
2020-05-09 18:13:01 -0700

    Add BrowserRouter to switch between views

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css'; 
 import NavBar from './components/NavBar';
 import MainContainer from './components/MainContainer';
+import ScenariosInfo from './components/ScenariosInfo';
+import Methods from './components/Methods';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <NavBar />
-      </div>
-      <MainContainer />
+      <BrowserRouter>
+        <main>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={MainContainer} />
+            <Route path="/scenarios" component={ScenariosInfo}/>
+            <Route path="/methods" component={Methods}/>
+          </Switch>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
