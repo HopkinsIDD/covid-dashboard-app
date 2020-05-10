162fa3525155a17a4c0816d93882674fecf0bbf5
Lily Xu <lilyxu@alum.mit.edu>
2020-05-09 18:11:31 -0700

    Add Methods and Scenarios Component for new views

---
+++ src/components/ScenariosInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ScenariosInfo extends Component {
+    render() {
+        return (
+          <div className="main-container">
+            <div className="container no-margin">
+              <h1>Scenarios</h1>
+              <div className="row"></div>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default ScenariosInfo;
