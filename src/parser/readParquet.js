9a7593fa6d1412c1edca2c92b67cfd393768b7a7
Lily Xu <lilyxu@alum.mit.edu>
2020-05-11 13:14:22 -0700

    Track issues with reading parquet via js

--- src/parser/readParquet.js
+++ src/parser/readParquet.js
@@ -3,7 +3,11 @@
 // https://github.com/ironSource/parquetjs
 var parquet = require('parquetjs');
 
-async function readData(path) {
+// also tried node-parquet but had issues with installation
+// node-parquet@0.2.7 preinstall: `./build_parquet-cpp.sh`
+// ./build_parquet-cpp.sh: line 19: cmake: command not found
+
+async function readParquet(path) {
     try {
         // create new ParquetReader that reads from 'fruits.parquet`
         let reader = await parquet.ParquetReader.openFile(path);
@@ -23,4 +27,4 @@ async function readData(path) {
 
 const fileName = 'high_death_death-000000001.hosp.parquet';
 const path = 'src/store/sims_pq/LockdownWideEffect/' + fileName;
-readData(path);
+readParquet(path);
