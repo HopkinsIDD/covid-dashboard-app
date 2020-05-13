256958d0709da21dbd27fb568407d58fa5b505fc
Lily Xu <lilyxu@alum.mit.edu>
2020-05-13 08:52:21 -0700

    Grab dates from the get-go for parser

--- src/parser/main.js
+++ src/parser/main.js
@@ -1,45 +1,43 @@
 // TO RUN FILE
 // remove "type": "module" from package.json (receive Error [ERR_REQUIRE_ESM])
 
-// TO TEST
-// shorten scenarios and files to run
-// remove simulation reduction to 30%
-
 // ASSUMPTIONS 
 // one scenario per directory with 3 severity levels 
 // file format looks like "high_death-1.csv"
 // severity is first word, sim number is last word
+// if sim cannot be determined from filename, will need to read every line of every
+// file to check for sim_num and whether or not it can be combed (or track
+// number of scenario/sev and cap it at 20 per say)
+// sim numbers are evenly distributed - reduction is based on divisibility
+// one sim per file
+
+// TO TEST
+// shorten scenarios and files to run
+// remove reduceSims function
 
 const fs = require('fs');
 const parse = require('./parse');
 const utils = require('./utils');
 const constants = require('./constants');
 const quant = require('./quantiles');
-const quantilesFile = require('../store/quant06085.json');
+const quantilesByGeoID = require('../store/quantilesByGeoID.json');
 
-function buildDataset() {
-    const dir = 'src/store/sims/';
-    const geoids = ['06085', '06019']; //'25017'; //'01081'; '06085'; // '06019'; // 
+function buildDataset(dir, geoids) {
+
     const scenarios = fs.readdirSync(dir)
-        .filter(file => file !== '.DS_Store')
-        .slice(0,1); 
+        .filter(file => file !== '.DS_Store');
+        //.slice(0,1); 
         
     const severities = constants.severities;
     const parameters = constants.parameters;
     const quantiles = constants.quantiles;
 
-    // faster to getDates from the get-go
-    let dates = [];
-    if (scenarios.length > 0) {
-        const files = fs.readdirSync(dir + scenarios[0] + '/',)
-            .filter(file => file !== '.DS_Store');
-        dates = utils.getDates(dir + scenarios[0] + '/' + files[0]);
-    } else {
-        console.log(`No scenario directories: ${dir}`)
-    }
+    // faster to grab dates from the get-go
+    const dates = utils.getDates(dir, scenarios);
 
     // TODO: readParquet.js
-    const parsedObj = parse.parseDirectories(dir,
+    const parsedObj = parse.parseDirectories(
+        dir,
         geoids,
         scenarios,
         severities,
@@ -54,22 +52,18 @@ function buildDataset() {
         severities,
         parameters,
         quantiles,
-        quantilesFile
+        quantilesByGeoID
     );
 
-    console.log('writing files...')
-    for (let g = 0; g < geoids.length; g ++) {
-        // write to file by geoid
-        const json = JSON.stringify(parsedObj[geoids[g]]);
-        const path = 'src/store/geo' + geoids[g] + '_test.json';
-
-        fs.writeFile(path, json, 'utf8', function(err) {
-            if (err) throw err;
-        });
-    }
+    utils.writeToFile(parsedObj, geoids);
+    
     console.log('end:', new Date());
     console.log('parse complete!'); 
 }
 
-buildDataset()
+const dir = 'src/store/sims/';
+const geoids = ['06085', '06019', '36061', '25017', '01081'];
+
+// todo: geoids should default to all unless specified for testing
+buildDataset(dir, geoids)
 
