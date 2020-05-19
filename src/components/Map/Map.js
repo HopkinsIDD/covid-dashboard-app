e5edbbdd317820c4697b0a32de07061676aad381
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-05-18 21:24:23 -0400

    hooking up color scale to counties

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-import { geoAlbersUsa, geoConicEqualArea, geoPath } from 'd3-geo';
+import { geoConicEqualArea, geoPath } from 'd3-geo';
+import { scaleLinear } from 'd3-scale';
+import { min, max } from 'd3-array';
 
 class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            maxVal: 0,
+            countyBoundaries: {}
+        }
+    }
     componentDidMount() {
         const { stat, dateIdx, countyBoundaries, statsForCounty } = this.props;
         console.log(stat);
@@ -9,27 +18,44 @@ class Map extends Component {
         console.log(countyBoundaries);
         console.log(statsForCounty);
 
+        // get max of all values in stat array for colorscale
+        const min = 0;
+        const maxVal = max(Object.values(statsForCounty).map( county => {
+            return max(county[stat])
+        }))
+        console.log(stat, maxVal)
+        
+
         // iterate over this.props.countyBoundaries to plot up boundaries
         // join each geoid to statsForCounty[geoid][stat][dateIdx]
+        for (let i = 0; i < countyBoundaries.features.length; i++) {
+            const geoid = countyBoundaries.features[i].properties.GEO_ID.slice(9)
+            // console.log(geoid)
+            countyBoundaries.features[i].properties[stat] = statsForCounty[geoid][stat]
+        }
+        console.log(countyBoundaries)
+        this.setState({ maxVal, countyBoundaries })
+        // console.log(Object.values(this.props.statsForCounty)[stat])
     }
 
     drawCounties = () => {
         const projection = geoConicEqualArea()
             .parallels([34, 40.5])
             .rotate([120, 0])
-            .fitSize([this.props.width, this.props.height], this.props.countyBoundaries)
+            .fitSize([this.props.width, this.props.height], this.state.countyBoundaries)
 
         const pathGenerator = geoPath().projection(projection)
+	    const ramp = scaleLinear().domain([ 0, this.state.maxVal ]).range(['#ffff00','#ff0000'])
 
-        const counties = this.props.countyBoundaries.features.map((d,i) => {
-            console.log(d)
+        const counties = this.state.countyBoundaries.features.map((d,i) => {
+            console.log(this.props.stat, d.properties[this.props.stat][this.props.dateIdx])
             return (<path
             key={`county-boundary-${i}`}
             d={pathGenerator(d)}
             style={{
                 stroke: 'lightgray',
-                fill: 'red',
-                fillOpacity: 0.5
+                fill: ramp(d.properties[this.props.stat][this.props.dateIdx]),
+                fillOpacity: 1
             }}
             className='counties'
         />)})
@@ -39,7 +65,19 @@ class Map extends Component {
     render() {
         return (
             <svg width={this.props.width} height={this.props.height}>
-                {this.drawCounties()}
+                <g style={{ stroke: '#00ff00'}}>
+                    <rect
+                        x={0}
+                        y={0}
+                        width={this.props.width}
+                        height={this.props.height}
+                        fillOpacity={0}
+                        stroke={'#00ff00'}
+                        strokeWidth='1'
+                    /> 
+                    {this.state.countyBoundaries.features && this.drawCounties()}
+                </g>
+                
             </svg>
         )
     }
