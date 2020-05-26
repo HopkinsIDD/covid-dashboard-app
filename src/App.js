a13f63f5d9817caf2964d95aec2964f41873c37f
Lily Xu <lilyxu@alum.mit.edu>
2020-05-26 10:07:08 -0700

    Link menu to anchor for single-page scrolling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css'; 
 import NavBar from './components/NavBar';
 import MainContainer from './components/MainContainer';
-import ScenariosInfo from './views/ScenariosInfo';
-import Methods from './views/Methods';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
         <main>
           <NavBar />
-          <Switch>
-            <Route path="/" exact component={MainContainer} />
-            <Route path="/scenarios" component={ScenariosInfo}/>
-            <Route path="/methods" component={Methods}/>
-          </Switch>
+          <MainContainer />
         </main>
-      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/TestDivider.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TestDivider extends Component {
+
+    render() {
+        return (
+            <div>
+                Greetings! I am a divider
+            </div>
+        )
+    }
+}
+
+export default TestDivider
