cd36fb20f57179de9c9bae6993db873c647f042b
Lily Xu <lilyxu@alum.mit.edu>
2020-05-27 09:13:48 -0700

    Set up testing framework

---
+++ src/parser/tests/utils.test.js
@@ -0,0 +1,122 @@
+const utils = require('../utils');
+const constants = require('../constants');
+
+test('returns expected dates array', () => {
+    const dir = 'src/parser/fixtures/';
+    const scenarios = ['USA_Lockdown1918'];
+
+    const expected = [
+        '2020-05-05', '2020-05-06', '2020-05-07', '2020-05-08', '2020-05-09'
+    ];
+    expect(utils.getDates(dir, scenarios)).toStrictEqual(expected);
+});
+
+test('returns expected initialized object', () => {
+    const geoids = ['06085', '06019', '36005', '36081'];
+    const scenarios = ['USA_Lockdown1918', 'USA_Uncontrolled'];
+    const dates = ['2020-05-05', '2020-05-06', '2020-05-07', '2020-05-08', '2020-05-09'];
+
+    const obj = utils.initObj(geoids, scenarios, dates);
+    const expected = require('../resources/expectedInitObj.json');
+
+    expect(obj).toStrictEqual(expected);
+})
+
+test('return expected index mapping', () => {
+    const headers = [
+        'time', 'uid', 'geoid', 'sim_num', 'comp', 'incidI', 'hosp_curr', 
+        'icu_curr', 'vent_curr', 'incidH', 'incidICU', 'incidVent', 
+        'incidD', 'date_inds', 'geo_ind'];
+
+    const obj = utils.getIdx(headers, constants.parameters);
+    const expected = require('../resources/expectedIdxMap.json');
+
+    expect(obj).toStrictEqual(expected);
+});
+
+// // TODO: need to mock constants.parameters inside aggregateByState to only be 'incidI'
+// // jest.mock('./constants', () => { parameters: ['incidI'] })
+
+// test('return expected aggregation by state', () => {
+//     const parsedObj = {
+//         '06085': {
+//             'USA_Uncontrolled': {
+//                 'high': {
+//                     'incidI': {
+//                         'peak': 0,
+//                         'sims': {
+//                             '1': [ 0, 1, 2, 5, 1 ], 
+//                             '10': [1, 3, 5, 9, 3 ]
+//                         },
+//                         'conf': {}
+//                     }
+//                 }
+//             }
+//         },
+//         '06019': {
+//             'USA_Uncontrolled': {
+//                 'high': {
+//                     'incidI': {
+//                         'peak': 0,
+//                         'sims': {
+//                             '1': [ 2, 2, 3, 7, 8 ], 
+//                             '10': [5, 8, 9, 9, 2 ]
+//                         },
+//                         'conf': {}
+//                     }
+//                 }
+//             }
+//         }
+//     };
+//     const scenarios = ['USA_Uncontrolled'];
+//     const dates = ['2020-05-05', '2020-05-06', '2020-05-07', '2020-05-08', '2020-05-09'];
+
+
+//     utils.aggregateByState(parsedObj, scenarios, dates);
+
+//     const expected = {
+//     '06': {
+//         'USA_Uncontrolled': {
+//             'high': {
+//                 'incidI': {
+//                     'peak': 0,
+//                     'sims': {
+//                         '1': [ 2, 3, 5, 12, 9 ], 
+//                         '10': [ 6, 11, 14, 18, 5 ]
+//                     },
+//                     'conf': {}
+//                 }
+//             }
+//         }
+//     },
+//     '06085': {
+//         'USA_Uncontrolled': {
+//             'high': {
+//                 'incidI': {
+//                     'peak': 0,
+//                     'sims': {
+//                         '1': [ 0, 1, 2, 5, 1 ], 
+//                         '10': [1, 3, 5, 9, 3 ]
+//                     },
+//                     'conf': {}
+//                 }
+//             }
+//         }
+//     },
+//     '06019': {
+//         'USA_Uncontrolled': {
+//             'high': {
+//                 'incidI': {
+//                     'peak': 0,
+//                     'sims': {
+//                         '1': [ 2, 2, 3, 7, 8 ], 
+//                         '10': [5, 8, 9, 9, 2 ]
+//                     },
+//                     'conf': {}
+//                 }
+//             }
+//         }
+//     }
+// };
+//     expect(parsedObj).toStrictEqual(expected);
+// });
