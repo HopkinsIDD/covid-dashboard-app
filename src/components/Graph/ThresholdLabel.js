f70feb669d75ed7ca4687a5a37d8ae1a0e14d4c3
Lily Xu <lilyxu@alum.mit.edu>
2020-05-27 15:03:37 -0700

    Prevent user from deselecting an R0 range

--- src/components/Graph/ThresholdLabel.js
+++ src/components/Graph/ThresholdLabel.js
@@ -6,15 +6,19 @@ function ThresholdLabel(props) {
     const val = addCommas(Math.ceil(props.statThreshold / 100) * 100);
     const date = getReadableDate(props.dateThreshold);
 
-    return (
-        <p className={props.classProps}>
-            <span className={props.statSliderActive || props.dateSliderActive ? 'customLink' : 'bold'}>{chance}%</span>
-            &nbsp;{`chance daily ${props.label} exceed`}&nbsp;
-            <span className={props.statSliderActive ? 'customLink' : 'bold'}>{val}</span>
-            &nbsp;by&nbsp;
-            <span className={props.dateSliderActive ? 'customLink' : 'bold'}>{date}</span>
-        </p>
+    if (props.r0.includes(0) && props.r0.includes(4)) {
+        return (
+            <p className={props.classProps}>
+                <span className={props.statSliderActive || props.dateSliderActive ? 'customLink' : 'bold'}>{chance}%</span>
+                &nbsp;{`chance daily ${props.label} exceed`}&nbsp;
+                <span className={props.statSliderActive ? 'customLink' : 'bold'}>{val}</span>
+                &nbsp;by&nbsp;
+                <span className={props.dateSliderActive ? 'customLink' : 'bold'}>{date}</span>
+            </p>
         )
+    } else {
+        return null;
+    }
 }
 
 export default ThresholdLabel
