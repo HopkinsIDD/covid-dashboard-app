aa9b56567f1afa568efd1f273720d04952d33133
Lily Xu <lilyxu@alum.mit.edu>
2020-05-27 09:14:34 -0700

    Add test for ParseSims

---
+++ src/parser/tests/parse.test.js
@@ -0,0 +1,33 @@
+// const fs = require('fs');
+const parse = require('../parse');
+const utils = require('../utils');
+// const constants = require('../constants');
+
+test('return correctly parsed obj for 1 simulation file', () => {
+    const filePath = 'src/parser/fixtures/USA_Lockdown1918/high_death-1.csv';
+    const scenario = 'USA_Lockdown1918';
+    const geoids = ['06085', '06019', '36005', '36081'];
+    const dates = ['2020-05-05', '2020-05-06', '2020-05-07', '2020-05-08', '2020-05-09'];
+    const severity = 'high'; 
+    
+    const getIdx = require('../resources/expectedIdxMap.json');
+    const expected = require('../resources/expectedParseSim.json');
+    
+    let result = utils.initObj(geoids, [scenario], dates);
+    parse.parseSim(filePath, result, geoids, scenario, severity, getIdx)
+    
+    expect(result).toStrictEqual(expected);
+});
+
+// test('return correctly parsed obj for scenario directories', () => {
+//     // Not returning what I expect
+//     const dir = 'src/parser/fixtures/';
+//     const geoids = ['06085', '06019', '36005', '36081'];
+//     const scenarios = ['USA_Lockdown1918', 'USA_Uncontrolled'];
+//     const dates = ['2020-05-05', '2020-05-06', '2020-05-07', '2020-05-08', '2020-05-09'];
+    
+//     const expected = require('../resources/expectedParseDirs.json');
+//     const result = parse.parseDirectories(dir, geoids, scenarios, dates)
+    
+//     expect(result).toStrictEqual(expected);
+// });
--- src/parser/utils.js
+++ src/parser/utils.js
@@ -2,248 +2,258 @@ const fs = require('fs');
 const _ = require('lodash');
 const constants = require('./constants');
 
-module.exports = {
+function initObj(geoids, scenarios, dates) {
+    // build structure of final Object
+    const obj = {};
 
-    initObj: function initObj(geoids, scenarios, dates) {
-        // build structure of final Object
-        const obj = {};
+    for (let geoid of geoids) {
+        obj[geoid] = {};
 
-        for (let geoid of geoids) {
-            obj[geoid] = {};
+        for (let scenario of scenarios) {
+            obj[geoid][scenario] = {'dates': dates};
 
-            for (let scenario of scenarios) {
-                obj[geoid][scenario] = {'dates': dates};
+            for (let sev of constants.severities ) {
+                obj[geoid][scenario][sev] = {};
     
-                for (let sev of constants.severities ) {
-                    obj[geoid][scenario][sev] = {};
-        
-                    for (let param of constants.parameters) {
-                        obj[geoid][scenario][sev][param] = {
-                            "peak": 0,
-                            "sims": {},
-                            "conf": {}
-                        };
-                    }    
-                }
+                for (let param of constants.parameters) {
+                    obj[geoid][scenario][sev][param] = {
+                        'peak': 0,
+                        'sims': {},
+                        'conf': {}
+                    };
+                }    
             }
         }
-        return obj;
-    },
+    }
+    return obj;
+}
+    
+function getIdx(headers, parameters) {
+    // build index mapping of selected parameters inside raw data headers
+    // assumes header order may change over time
 
-    getIdx: function getIdx(headers, parameters) {
-        // build index mapping of selected parameters inside raw data headers
-        // assumes header order may change over time
+    let getIdx = Object();
+    getIdx['geoid'] = headers.indexOf('geoid')
+    getIdx['sim_num'] = headers.indexOf('sim_num')
+    getIdx['time'] = headers.indexOf('time')
 
-        let getIdx = Object();
-        getIdx['geoid'] = headers.indexOf('geoid')
-        getIdx['sim_num'] = headers.indexOf('sim_num')
-        getIdx['time'] = headers.indexOf('time')
+    for (let i = 0; i < parameters.length; i ++) {
+        getIdx[parameters[i]] = headers.indexOf(parameters[i])
+    }
+    return getIdx;
+}
 
-        for (let i = 0; i < parameters.length; i ++) {
-            getIdx[parameters[i]] = headers.indexOf(parameters[i])
-        }
-        return getIdx;
-    },
+function getDates(dir, scenarios) {
+    // parameter dir is entire model package directory
+    // return Array of dates 
 
+    const files = fs.readdirSync(dir + scenarios[0] + '/',)
+        .filter(file => file !== '.DS_Store');
 
-    getDates: function getDates(dir, scenarios) {
-        // parameter dir is entire model package directory
-        // return Array of dates 
+    let dates = new Set();
+    try {
+        const path = dir + scenarios[0] + '/' + files[0]
+        const data = fs.readFileSync(path, 'UTF-8');
+        const lines = data.split(/\r?\n/);
 
-        const files = fs.readdirSync(dir + scenarios[0] + '/',)
-            .filter(file => file !== '.DS_Store');
+        lines.forEach((line) => {
+            const splitLine = line.split(',')
+            const date = splitLine[0]
 
-        let dates = new Set();
-        try {
-            const path = dir + scenarios[0] + '/' + files[0]
-            const data = fs.readFileSync(path, 'UTF-8');
-            const lines = data.split(/\r?\n/);
+            // skip header and empty lines
+            if (date.length > 1 && date !== 'time') {
+                dates.add(date);
+            }
+        });
+    } catch (err) {
+        console.error(`No scenario directories: ${dir}`);
+    }
+    return Array.from(dates).sort();
+}
 
-            lines.forEach((line) => {
-                const splitLine = line.split(',')
-                const date = splitLine[0]
+function getHeaders(path) {
+    // return Array of headers 
+    let headers = [];
 
-                // skip header and empty lines
-                if (date.length > 1 && date !== 'time') {
-                    dates.add(date);
+    try {
+        const data = fs.readFileSync(path, 'UTF-8');
+        const lines = data.split(/\r?\n/);
+
+        lines.forEach((line) => {
+            const splitLine = line.split(',')
+            const date = splitLine[0]
+            if (date === 'time') {
+                for (let i = 0; i < splitLine.length; i ++) {
+                    headers.push(splitLine[i]);
                 }
-            });
-        } catch (err) {
-            console.error(`No scenario directories: ${dir}`);
-        }
-        return Array.from(dates).sort();
-    },
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+    return headers;
+}
 
-    getHeaders: function getHeaders(path) {
-        // return Array of headers 
-        let headers = [];
+function notHeaderOrEmpty(line) {
+    // return true if line is not header or empty lines at end
 
-        try {
-            const data = fs.readFileSync(path, 'UTF-8');
-            const lines = data.split(/\r?\n/);
+    const splitLine = line.split(',')
+    const date = splitLine[0];
 
-            lines.forEach((line) => {
-                const splitLine = line.split(',')
-                const date = splitLine[0]
-                if (date === 'time') {
-                    for (let i = 0; i < splitLine.length; i ++) {
-                        headers.push(splitLine[i]);
-                    }
-                }
-            });
-        } catch (err) {
-            console.error(err);
-        }
-        return headers;
-    },
+    return date.length > 1 && date !== 'time'
 
-    notHeaderOrEmpty: function notHeaderOrEmpty(line) {
-        // return true if line is not header or empty lines at end
+}
 
-        const splitLine = line.split(',')
-        const date = splitLine[0];
+function returnFilesBySev(files, severity) {
+    // returns Array of file names for given severity type
 
-        return date.length > 1 && date !== 'time'
-
-    },
-
-    returnFilesBySev: function returnFilesBySev(files, severity) {
-        // returns Array of file names for given severity type
-
-        const filesBySev = [];
-        for (let i = 0; i < files.length; i++) {
-            if (files[i].includes(severity)) {
-                filesBySev.push(files[i]);
-            };
+    const filesBySev = [];
+    for (let i = 0; i < files.length; i++) {
+        if (files[i].includes(severity)) {
+            filesBySev.push(files[i]);
         };
-        return filesBySev;
-    },
+    };
+    return filesBySev;
+}
 
-    aggregateByState: function aggregateByState(parsedObj, scenarios, dates) {
-        // add state geoid with sims aggregated by county geoid to parsedObj
+function aggregateByState(parsedObj, scenarios, dates) {
+    // add state geoid with sims aggregated by county geoid to parsedObj
 
-        const allGeoids = Object.keys(parsedObj);
-        const states = [...new Set(allGeoids.map(geoid => geoid.slice(0, 2)))];
-        const finalObj = module.exports.initObj(states, scenarios, dates);
+    const allGeoids = Object.keys(parsedObj);
+    const states = [...new Set(allGeoids.map(geoid => geoid.slice(0, 2)))];
+    const finalObj = module.exports.initObj(states, scenarios, dates);
 
-        for (let state of states) {
+    for (let state of states) {
 
-            const geoids = allGeoids.filter(g => g.slice(0, 2) === state);
-            for (let geoid of geoids) { 
+        const geoids = allGeoids.filter(g => g.slice(0, 2) === state);
+        for (let geoid of geoids) { 
 
-                for (let scen of scenarios) {
+            for (let scen of scenarios) {
 
-                    for (let sev of constants.severities ) {
+                for (let sev of constants.severities ) {
 
-                        for (let param of constants.parameters) {
+                    for (let param of constants.parameters) {
 
-                            if (Object.keys(finalObj[state][scen][sev][param].sims).length === 0) {
-                                finalObj[state][scen][sev][param].sims = _.cloneDeep(parsedObj[geoid][scen][sev][param].sims);
+                        if (Object.keys(finalObj[state][scen][sev][param].sims).length === 0) {
+                            finalObj[state][scen][sev][param].sims = _.cloneDeep(parsedObj[geoid][scen][sev][param].sims);
 
-                            } else {
-                                const sims = Object.keys(parsedObj[geoid][scen][sev][param].sims);
+                        } else {
+                            const sims = Object.keys(parsedObj[geoid][scen][sev][param].sims);
 
-                                for (let sim of sims) {
+                            for (let sim of sims) {
 
-                                    for (let i = 0 ; i < dates.length; i++) {
-                                        const val = parsedObj[geoid][scen][sev][param].sims[sim][i]; 
-                                        finalObj[state][scen][sev][param].sims[sim][i] = finalObj[state][scen][sev][param].sims[sim][i] + val;
-                                    }
+                                for (let i = 0 ; i < dates.length; i++) {
+                                    const val = parsedObj[geoid][scen][sev][param].sims[sim][i]; 
+                                    finalObj[state][scen][sev][param].sims[sim][i] = finalObj[state][scen][sev][param].sims[sim][i] + val;
                                 }
                             }
+                        }
 
-                        }     
-                    }   
-                }
-            }
-            parsedObj[state] = finalObj[state];
-        }
-    },
-
-    calcReduceInt: function calcReduceInt(fileLength) {
-        // returns int a sim number must be divisible by
-        // in order to be included in final dataset
-        // fileLength is length of files in scenario directory
-        // includes all severities
-
-        if (fileLength < 60) {
-            return 1;
-        } else if (fileLength <= 120) {
-            return 2;
-        } else if (fileLength <= 200) {
-            return 3;
-        } else if (fileLength <= 300) {
-            return 4;
-        } else {
-            return 5;
-        }
-    },
-
-    reduceSims: function reduceSims(dir, parsedObj) {
-        // reduce number of sims based on sim files per scenario
-        
-        const geoids = Object.keys(parsedObj);
-        for (let geoid of geoids) {
-    
-            const scenarios = Object.keys(parsedObj[geoid]);
-            for (let scenario of scenarios) {
-
-                const files = fs.readdirSync(`${dir}${scenario}/`)
-                    .filter(file => file !== '.DS_Store');
-                const reduceInt = module.exports.calcReduceInt(files.length);
-                for (let sev of constants.severities) {
-
-                    for (let param of constants.parameters) {
-
-                        const newSims = parsedObj[geoid][scenario][sev][param].sims
-                            .filter(sim => sim.name % reduceInt === 0);
-                        parsedObj[geoid][scenario][sev][param].sims = newSims;
-                    }
-                }
+                    }     
+                }   
             }
         }
-    },
+        parsedObj[state] = finalObj[state];
+    }
+}
 
-    writeToFile: function writeToFile(parsedObj) {
-        // each geoid will write to separate JSON file
+function calcReduceInt(fileLength) {
+    // returns int a sim number must be divisible by
+    // in order to be included in final dataset
+    // fileLength is length of files in scenario directory
+    // includes all severities
 
-        const geoids = Object.keys(parsedObj);
-        console.log('... writing files')
-        for (let g = 0; g < geoids.length; g ++) {
-            const json = JSON.stringify(parsedObj[geoids[g]]);
-            const path = `src/store/geo${geoids[g]}.json`;
-    
-            fs.writeFileSync(path, json, 'utf8', function(err) {
-                if (err) throw err;
-            });
-        }
-    },
+    if (fileLength < 60) {
+        return 1;
+    } else if (fileLength <= 120) {
+        return 2;
+    } else if (fileLength <= 200) {
+        return 3;
+    } else if (fileLength <= 300) {
+        return 4;
+    } else {
+        return 5;
+    }
+}
 
-    combineCaliCounties: function combineCaliCounties() {
-        // Add renamed scenarios of geo06019 to geo06085
+function reduceSims(dir, parsedObj) {
+    // reduce number of sims based on sim files per scenario
     
-        const geo06019 = require('../store/geo06019.json');
-        const oldKeys = Object.keys(geo06019);
-        const newKeys = ['USA_Lockdown1945', 'USA_LockdownHK', 'USA_Fatiguing'];
-        
-        // rename scenarios of geo06019
-        for (let i = 0; i < oldKeys.length; i ++) {
-            Object.defineProperty(geo06019, newKeys[i],
-                Object.getOwnPropertyDescriptor(geo06019, oldKeys[i]));
-            delete geo06019[oldKeys[i]];
+    const geoids = Object.keys(parsedObj);
+    for (let geoid of geoids) {
+
+        const scenarios = Object.keys(parsedObj[geoid]);
+        for (let scenario of scenarios) {
+
+            const files = fs.readdirSync(`${dir}${scenario}/`)
+                .filter(file => file !== '.DS_Store');
+            const reduceInt = module.exports.calcReduceInt(files.length);
+            for (let sev of constants.severities) {
+
+                for (let param of constants.parameters) {
+
+                    const newSims = parsedObj[geoid][scenario][sev][param].sims
+                        .filter(sim => sim.name % reduceInt === 0);
+                    parsedObj[geoid][scenario][sev][param].sims = newSims;
+                }
+            }
         }
-    
-        // add renamed scenarios to geo06085
-        const geo06085 = require('../store/geo06085.json');
-        for (let key of newKeys) {
-            geo06085[key] = geo06019[key];
-        }
-        
-        const json = JSON.stringify(geo06085);
-        const path = `src/store/geo06085.json`;
-    
+    }
+}
+
+function writeToFile(parsedObj, geoids) {
+    // each geoid will write to separate JSON file
+
+    // const geoids = Object.keys(parsedObj);
+    console.log('... writing files')
+    for (let g = 0; g < geoids.length; g ++) {
+        const json = JSON.stringify(parsedObj[geoids[g]]);
+        const path = `src/store/geo${geoids[g]}.json`;
+
         fs.writeFileSync(path, json, 'utf8', function(err) {
             if (err) throw err;
         });
     }
+}
+
+function combineCaliCounties() {
+    // Add renamed scenarios of geo06019 to geo06085
+
+    const geo06019 = require('../store/geo06019.json');
+    const oldKeys = Object.keys(geo06019);
+    const newKeys = ['USA_Lockdown1945', 'USA_LockdownHK', 'USA_Fatiguing'];
+    
+    // rename scenarios of geo06019
+    for (let i = 0; i < oldKeys.length; i ++) {
+        Object.defineProperty(geo06019, newKeys[i],
+            Object.getOwnPropertyDescriptor(geo06019, oldKeys[i]));
+        delete geo06019[oldKeys[i]];
+    }
+
+    // add renamed scenarios to geo06085
+    const geo06085 = require('../store/geo06085.json');
+    for (let key of newKeys) {
+        geo06085[key] = geo06019[key];
+    }
+    
+    const json = JSON.stringify(geo06085);
+    const path = `src/store/geo06085.json`;
+
+    fs.writeFileSync(path, json, 'utf8', function(err) {
+        if (err) throw err;
+    });
+}
+
+module.exports = {
+    initObj,
+    getIdx,
+    getDates,
+    getHeaders,
+    notHeaderOrEmpty,
+    returnFilesBySev,
+    aggregateByState,
+    calcReduceInt,
+    reduceSims,
+    writeToFile,
+    combineCaliCounties
 }
