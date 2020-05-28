af672b352047465685cb9abdf2a90100ab576fc8
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-05-28 14:56:40 -0400

    adding svg icons to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
+import { ReactComponent as GraphLogo } from '../assets/graph.svg';
+import { ReactComponent as ChartLogo } from '../assets/chart.svg';
+import { ReactComponent as MapLogo } from '../assets/globe.svg';
+import { ReactComponent as MethodsLogo } from '../assets/book.svg';
 
 class NavBar extends Component {
     render() {
@@ -10,10 +14,11 @@ class NavBar extends Component {
                 <h2 className="titleNarrow siteTitle">COVID-19 Intervention Scenarios</h2>
               </Col>
               <Col className="gutter-row nav-menu" span={8}>
-                <ul>
-                  <li><a href="#scenario-comparisons">VIZ</a></li>
-                  <li><a href="#stats">STATS</a></li>
-                  <li><a href="#map">MAP</a></li>
+                <ul style={{ marginTop: '5px' }}>
+                  <li style={{ paddingRight: '20px'}}><a href="#scenario-comparisons"><GraphLogo height="28" /></a></li>
+                  <li style={{ paddingRight: '20px'}}><a href="#stats"><ChartLogo height="28"/></a></li>
+                  <li style={{ paddingRight: '30px'}}><a href="#map"><MapLogo height="28"/></a></li>
+                  <li style={ {paddingRight: '20px'}}><a href="#methods"><MethodsLogo height="28"/></a></li>
                 </ul>
               </Col>
             </Row>
