ad8db658bba5c5ff3c68726f1e21be0de00424bd
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-05-30 10:18:59 -0400

    got svg menu hover and active state working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,48 +2,29 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import logo from '../assets/logo.png'; 
 import brand from '../assets/idd.png'; 
-import { ReactComponent as GraphLogo } from '../assets/graph.svg';
-import { ReactComponent as ChartLogo } from '../assets/chart.svg';
-import { ReactComponent as MapLogo } from '../assets/globe.svg';
-import { ReactComponent as MethodsLogo } from '../assets/book.svg';
+import MenuItem from './MenuItem';
 import { select } from 'd3-selection';
 // import { ReactComponent as JHLogo } from '../assets/logo-john-hopkins.svg';
 import { green } from '../utils/constants.js'
+import { isThisTypeNode } from 'typescript';
+
 class NavBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       active: null,
-      logos: [<GraphLogo 
-        height="24" 
-        className="nav-hover"
-        onClick={() => this.handleMouseClick}
-        />, <ChartLogo 
-        height="24"
-        className="nav-hover"
-        onClick={() => this.handleMouseClick}
-        />, <MapLogo 
-        height="24"
-        className="nav-hover"
-        onClick={() => this.handleMouseClick}
-      />, <MethodsLogo
-        height="24"
-        className="nav-hover-methods"
-        onClick={() => this.handleMouseClick}
-      /> 
-      ],
       links: ['#scenario-comparisons', '#stats', '#map', '#methods']
     }
   }
 
-  handleMouseClick = (menuItem) => {
-    console.log('clicked')
-    console.log(menuItem)
-    this.setState({ active: menuItem })
-    // select(event.target).attr("class", "nav-active")
+  handleMouseClick = (index) => {
+    // console.log('clicked')
+    // console.log(index)
+    this.setState({ active: index })
   }
 
   render() {
+    console.log(this.state.active)
     return (
       <div id="navbar" className="App-header">
         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
@@ -52,15 +33,22 @@ class NavBar extends Component {
           </Col>
           <Col className="gutter-row mini-search" span={6}>
             <img className="brand" src={brand} alt="Brand" />
-            {/* <JHLogo height="24" throwIfNamespace={false} /> */}
           </Col>
           <Col className="gutter-row nav-menu" offset={7} span={10}>
             <ul style={{ marginTop: '5px' }}>
-              {this.state.logos.map( (logo, index) => {
+              {this.state.links.map( (link, index) => {
+                console.log(index, 'active', this.state.active === index)
                 return (
-                  <li style={{ paddingRight: (index < this.state.logos.length - 1) ? '10px': '0px' }}>
-                    <a href={this.state.links[index]}>
-                      {logo}
+                  <li style={{ paddingRight: (index < this.state.links.length - 1) ? '10px': '0px' }}>
+                    <a href={link}>
+                      <MenuItem  
+                        height="24"
+                        active={this.state.active === index ? true : false}
+                        activeClass={index === 3 ? "nav-active-methods" : "nav-active"}
+                        hoverClass={index === 3 ? "nav-hover-methods" : "nav-hover"}
+                        handleMouseClick={this.handleMouseClick}
+                        menuItem={index}
+                      />
                     </a>
                   </li>
                 )
@@ -74,3 +62,4 @@ class NavBar extends Component {
 }
 
 export default NavBar;
+
