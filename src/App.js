860c8f694a3958632c5024164e996ce92b8d9f48
Lily Xu <lilyxu@alum.mit.edu>
2020-05-29 17:01:19 -0700

    Lift NavBar out of MainContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css'; 
 import MainContainer from './components/MainContainer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
         <main>
+          <NavBar />
           <MainContainer />
         </main>
     </div>
