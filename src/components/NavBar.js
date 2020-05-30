e03c04e640a32d337cd8f9b68a9c45fcc7f33fc4
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-05-30 01:20:12 -0400

    starting to do nav implementation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,20 +6,41 @@ import { ReactComponent as GraphLogo } from '../assets/graph.svg';
 import { ReactComponent as ChartLogo } from '../assets/chart.svg';
 import { ReactComponent as MapLogo } from '../assets/globe.svg';
 import { ReactComponent as MethodsLogo } from '../assets/book.svg';
+import { select } from 'd3-selection';
 // import { ReactComponent as JHLogo } from '../assets/logo-john-hopkins.svg';
 import { green } from '../utils/constants.js'
 class NavBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hovered: null,
       active: null,
-      class: ''
+      logos: [<GraphLogo 
+        height="24" 
+        className="nav-hover"
+        onClick={() => this.handleMouseClick}
+        />, <ChartLogo 
+        height="24"
+        className="nav-hover"
+        onClick={() => this.handleMouseClick}
+        />, <MapLogo 
+        height="24"
+        className="nav-hover"
+        onClick={() => this.handleMouseClick}
+      />, <MethodsLogo
+        height="24"
+        className="nav-hover-methods"
+        onClick={() => this.handleMouseClick}
+      /> 
+      ],
+      links: ['#scenario-comparisons', '#stats', '#map', '#methods']
     }
   }
 
-  handleClick = () => {
-
+  handleMouseClick = (menuItem) => {
+    console.log('clicked')
+    console.log(menuItem)
+    this.setState({ active: menuItem })
+    // select(event.target).attr("class", "nav-active")
   }
 
   render() {
@@ -35,42 +56,15 @@ class NavBar extends Component {
           </Col>
           <Col className="gutter-row nav-menu" offset={7} span={10}>
             <ul style={{ marginTop: '5px' }}>
-              <li style={{ paddingRight: '10px'}}>
-                <a href="#scenario-comparisons">
-                  <GraphLogo 
-                    height="24" 
-                    className="nav-hover"
-                    onMouseClick={this.handleMouseClick}
-                    />
-                </a>
-              </li>
-              <li style={{ paddingRight: '10px'}}>
-                <a href="#stats">
-                  <ChartLogo 
-                  height="24"
-                  className="nav-hover"
-                  onMouseClick={this.handleMouseClick}
-                  />
-                </a>
-              </li>
-              <li style={{ paddingRight: '20px'}}>
-                <a href="#map">
-                  <MapLogo 
-                  height="24"
-                  className="nav-hover"
-                  onMouseClick={this.handleMouseClick}
-                  />
-                </a>
-              </li>
-              <li>
-                <a href="#methods">
-                  <MethodsLogo
-                    height="24"
-                    className="nav-hover-methods"
-                    onMouseClick={this.handleMouseClick}
-                  />
-                </a>
-              </li>
+              {this.state.logos.map( (logo, index) => {
+                return (
+                  <li style={{ paddingRight: (index < this.state.logos.length - 1) ? '10px': '0px' }}>
+                    <a href={this.state.links[index]}>
+                      {logo}
+                    </a>
+                  </li>
+                )
+              })}
             </ul>
           </Col>
         </Row>
