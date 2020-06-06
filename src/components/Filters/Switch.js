48d3ca12b3a4154c01e6f6475fee19593ac8c8a7
Lily Xu <lilyxu@alum.mit.edu>
2020-06-05 17:18:29 -0700

    Add tooltip option

--- src/components/Filters/Switch.js
+++ src/components/Filters/Switch.js
@@ -1,8 +1,20 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import { Radio } from 'antd';
+// import TooltipHandler from '../Filters/TooltipHandler';
 // import { styles } from '../../utils/constants';
 
-class Switch extends PureComponent {
+class Switch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showTooltip: false
+        }
+    }
+
+    handleTooltipClick = () => {
+        this.setState({showTooltip: !this.state.showTooltip})
+    }
+
     render() {
         const value = this.props.showConfBounds ? "confidence" : "exceedence";
         return (    
@@ -22,6 +34,22 @@ class Switch extends PureComponent {
                     value="confidence">
                     Confidence
                 </Radio.Button>
+                {/* <TooltipHandler
+                    showTooltip={this.state.showTooltip}
+                    onClick={this.handleTooltipClick}
+                    >
+                    <div className="tooltip">&nbsp;&#9432;
+                        {this.state.showTooltip ?
+                        <span className="tooltip-text">
+                            The boundaries in the graph display the 10%, 50%, 
+                            and 90% confidence intervals. 
+                            Confidence intervals are
+                            an estimate of the population based on the 
+                            based on the entire set of simulation curves.  
+                        </span> 
+                        : null}
+                    </div>
+                </TooltipHandler> */}
             </Radio.Group> 
         )
     }
