4713ec7478db558652f5cfdf575a01363ece5451
Christopher Lee <clee@truerowing.com>
2020-06-11 00:12:17 -0400

    Refactor colors (#48)
    * Refactor colors
    
    * adding chartBkgd to colors file
    
    * Add chartBkgd to colors, colors.gray in Graph
    
    Co-authored-by: Genevieve Hoffman <genny.hoffman@gmail.com>
    Co-authored-by: Lily Xu <lilyxu@alum.mit.edu>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,16 +3,6 @@
 // name: display name for visualization
 
 export const margin = { yAxis: 80, top: 10, right: 10, bottom: 30, left: 10, chartTop: 15 };
-export const red = '#d31d30';
-export const green = '#4ddaba';
-export const blue = '#1f90db';
-export const gray = '#9b9b9b';
-export const lightgray = '#d0d0d0';
-export const graphBkgd = '#fcfcfc';
-export const chartBkgd = '#f0f2f5';
-export const scenarioColors = ['#4ddaba', '#b6f5e7','#19b18e', '#e0fdf7', '#008769', '#acdacf']
-export const mapLowColors = ['#deebf7', '#E0F5F0', '#FFEDE3', '#FFE6EA']
-export const mapHighColors = ['#3885fa', '#148C81', '#E6500D', '#DE264D']
 
 // ant design components are inline styled (and resist styling via css classes)
 export const styles = {
