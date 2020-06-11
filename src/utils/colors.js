5fd660dc5278a71604f1f821986f481eda5af366
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-06-10 22:42:17 -0400

    adding chartBkgd to colors file

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -18,6 +18,7 @@ export const colors = {
     gray: '#9b9b9b',
     lightGray: '#d0d0d0',
     graphBkgd: '#fcfcfc',
+    chartBkgd: '#f0f2f5',
 
     // Names below are pulled from https://www.colorhexa.com. Duplicates are incremented
     verySoftCyan: '#b6f5e7',
