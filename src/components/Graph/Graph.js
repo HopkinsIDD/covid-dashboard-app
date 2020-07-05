5175872ff6da19ce4ea79596c5006def146f4760
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-07-05 17:54:11 -0400

    removing consoling from graph

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -59,6 +59,7 @@ class Graph extends Component {
         }
 
         if (series !== prevProps.series) {
+            console.log('componentDidUpdate series change')
             const { lineGenerator, areaGenerator } = prevState;
             this.updateSimPaths(series, selectedDates, lineGenerator, animateTransition, width);
             if (confBounds && confBounds.length > 0) {
@@ -67,6 +68,7 @@ class Graph extends Component {
 
         const { xScale, yScale } = this.props;
         if (xScale !== prevProps.xScale || yScale !== prevProps.yScale) {
+            console.log('componentDidUpdate scale change')
             const { lineGenerator, areaGenerator } = prevState;
             this.updateSimPaths(series, selectedDates, lineGenerator, animateTransition, width);
             if (confBounds && confBounds.length > 0) {
