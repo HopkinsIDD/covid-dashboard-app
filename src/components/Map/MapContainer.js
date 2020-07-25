b3f5539e0ca0705b2c85fed251c2529c56bd6336
Genevieve Hoffman <genny.hoffman@gmail.com>
2020-07-25 16:01:31 -0400

    lifting zoom out of Map into MapContainer

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -4,7 +4,8 @@ import { STATS } from '../../utils/constants';
 import { getDateIdx, getReadableDate } from '../../utils/utils';
 import { COUNTYNAMES } from '../../utils/geoids';
 import { mapHighColorPalette, mapLowColorPalette } from '../../utils/colors';
-
+import { scalePow } from 'd3-scale';
+import { select } from 'd3-selection';
 
 class MapContainer extends Component {
     constructor(props) {
@@ -12,8 +13,16 @@ class MapContainer extends Component {
         this.state = {
             children: [],
             scaleColors: [],
-            numMaps: 4 // number of individual maps to display in Map View
+            numMaps: 4, // number of individual maps to display in Map View
+            strokeWidth: 0.8,
+            strokeHoverWidth: 1.8
         }
+        this.mapRefContainer = React.createRef();
+        
+        this.strokeWidthScale = scalePow().exponent(0.25)
+            .range([0.1, 0.8]).domain([9, 1])
+        this.strokeHoverWidthScale = scalePow().exponent(0.25)
+            .range([0.25, 1.8]).domain([9, 1])
     }
   
     componentDidMount() {
@@ -38,7 +47,7 @@ class MapContainer extends Component {
     initializeMaps = (geoid, scenario, firstDate, selectedDate, width, height) => {
         const children = [];
         const dateIdx = getDateIdx(firstDate, selectedDate);
-        const { numMaps } = this.state;
+        const { numMaps, strokeWidth, strokeHoverWidth } = this.state;
 
         for (let stat of STATS.slice(0, numMaps)) {
             const child = {
@@ -58,11 +67,26 @@ class MapContainer extends Component {
                     height={height}
                     lowColor={mapLowColorPalette[stat.id]}
                     highColor={mapHighColorPalette[stat.id]}
+                    strokeWidth={strokeWidth}
+                    strokeHoverWidth={strokeHoverWidth}
+                    handleZoom={this.handleZoom}
                 />
             ) 
             children.push(child);
         }
         this.setState({ children });
+    }
+
+    handleZoom = (event) => {
+        // console.log(event)
+        if (this.mapRefContainer.current) {
+            const mapNode = select(this.mapRefContainer.current)
+            mapNode.selectAll('path')
+                .attr('transform', event.transform)
+            const strokeWidth = this.strokeWidthScale(event.transform.k)
+            const strokeHoverWidth = this.strokeHoverWidthScale(event.transform.k)
+            this.setState({ strokeWidth, strokeHoverWidth })
+        }
     }
 
     render() {
@@ -78,7 +102,7 @@ class MapContainer extends Component {
                         {getReadableDate(this.props.selectedDate)}
                     </span>
                 </div>
-                <div className="map-wrapper">
+                <div className="map-wrapper" ref={this.mapRefContainer}>
                     {this.state.children.map(child => {
                         return (
                             <div className="map" key={child.key}>
