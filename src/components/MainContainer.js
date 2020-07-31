44e18d611f268180773ed04193ed0bdcfcfb4a76
Lily Xu <lilyxu@alum.mit.edu>
2020-07-30 17:38:28 -0700

    Access s3 obj from same bucket, add parsing

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,7 +5,7 @@ import { margin, dimMultipliers } from '../utils/constants';
 import Search from './Search/Search'
 import MainGraph from './Graph/MainGraph';
 import MainChart from './Chart/MainChart';
-import MainMap from './Map/MainMap';
+// import MainMap from './Map/MainMap';
 import Methodology from './Methodology';
 import About from './About';
 
@@ -14,7 +14,7 @@ const dataset = require('../store/06085.json');
 const aws = require('aws-sdk');
 const config = require('../config.json');
 
-async function getS3Obj() {
+async function getS3Obj(geoid) {
     try {
         aws.config.setPromisesDependency();
         aws.config.update({
@@ -24,16 +24,16 @@ async function getS3Obj() {
         })
 
         const s3 = new aws.S3();
-        const response = await s3.listObjectsV2({
-            Bucket: 'covid-geoids'
+        const response = await s3.getObject({
+            Bucket: 'covid-scenario-dashboard',
+            Key: `json-files/${geoid}.json`
         }).promise();
 
-        console.log(response);
+        return JSON.parse(response.Body.toString('utf-8'));
 
     } catch (e) {
         console.log('our error', e)
     }
-    debugger;
 }
 
 class MainContainer extends Component {
@@ -69,7 +69,9 @@ class MainContainer extends Component {
 
     updateGraphDimensions = () => {
         const ratioH = dimMultipliers.graphDesktopH;
-        const ratioW = window.innerWidth > 800 ? dimMultipliers.graphDesktopW : dimMultipliers.graphMobileW; // account for mobile
+        const ratioW = window.innerWidth > 800 ? 
+            dimMultipliers.graphDesktopW : 
+            dimMultipliers.graphMobileW; // account for mobile
 
         const graphH = window.innerHeight * ratioH;
         const graphW = (window.innerWidth * ratioW) - margin.yAxis; 
@@ -79,18 +81,19 @@ class MainContainer extends Component {
 
     updateMapContainerDimensions = () => {
         const ratioH = dimMultipliers.mapDesktopH;
-        const ratioW = window.innerWidth > 800 ? dimMultipliers.graphDesktopW : dimMultipliers.mapMobileW; // account for mobile 
+        const ratioW = window.innerWidth > 800 ? 
+            dimMultipliers.graphDesktopW : 
+            dimMultipliers.mapMobileW; // account for mobile 
 
         const mapContainerH = window.innerHeight * ratioH;
-        const mapContainerW = ((window.innerWidth * ratioW) - margin.yAxis) - (6 * (margin.left))
+        const mapContainerW = ((window.innerWidth * ratioW) - margin.yAxis) - 
+            (6 * (margin.left));
         
         this.setState({ mapContainerW, mapContainerH });
     }
 
     handleCountySelect = (geoid) => {
-        // const dataset = require(`../store/${geoid}.json`);
-        getS3Obj()
-        // this.setState({dataset, geoid})
+        getS3Obj(geoid).then(dataset => this.setState({dataset, geoid}));
     };
     
     handleUpload = (dataset, geoid) => {
@@ -123,13 +126,13 @@ class MainContainer extends Component {
                     height={this.state.graphH * dimMultipliers.chartDesktopH} 
                 />}
 
-                {this.state.dataLoaded &&
+                {/* {this.state.dataLoaded &&
                 <MainMap
                     geoid={this.state.geoid}
                     dataset={this.state.dataset}
                     width={this.state.mapContainerW - margin.left - margin.right}
                     height={this.state.mapContainerH}
-                />}
+                />} */}
                 
                 <Methodology />
                 <About />
