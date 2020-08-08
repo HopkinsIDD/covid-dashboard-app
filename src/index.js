1d98ddcba1d96c6aa033d042a3450c7eb482314b
Christopher Lee <clee@truerowing.com>
2020-08-08 12:07:15 -0400

    Add redux to app

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
