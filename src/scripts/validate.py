3003123772218e42deb0f80309a529341553ff7a
Lily Xu <lilyxu@alum.mit.edu>
2020-09-09 10:33:19 -0700

    Validation for actuals, boundaries, and map

--- src/scripts/validate.py
+++ src/scripts/validate.py
@@ -62,7 +62,6 @@ def validate_sim_keys(scenario: str, rate: str, indicator: str, sim_obj: dict, e
 
 def validate_geoid(obj: dict, outcomes: dict):
     ''' Validate one county geoid dict object '''
-    
     scenarios = list(obj.keys())
     for scenario in scenarios:
 
@@ -84,53 +83,161 @@ def validate_geoid(obj: dict, outcomes: dict):
                 validate_sim_keys(scenario, rate, indicator, sim_obj, expected_days)
 
 
-def validate_stats_for_map(stats: dict, expected_states: list, expected_counties: list):
-
+def validate_map_stats(stats: dict, expected_states: list, expected_counties: list, outcomes: dict) -> set:
+    ''' 
+    Validate statsForMap includes all expected states, counties, and outcomes
+    Returns scenario names to compare against scenario names in geoid files
+    '''
     states = sorted(list(stats.keys()))
-    for state in expected_states[:57]:
+    expected_indicators = set(value['key'] for value in outcomes.values())
+    scenario_names = []
+    
+    for state in expected_states: 
         if state not in states:
             raise Exception("statsForMap.json is missing state {}".format(state))
 
+        exp_counties = [x for x in expected_counties if x.startswith(state)]
+        counties = sorted(list(stats[state].keys()))
 
-    print('hi!')
+        for county in exp_counties:
+            if county not in counties:
+                # error, not exception, since web app can handle missing counties in statsForMap
+                print("ERROR: statsForMap state {} is missing county {}".format(state, county))
+            else:
+                scenarios = sorted(list(stats[state][county].keys()))
+                scenario_names = scenarios if not scenario_names else scenario_names
+                indicators = set(stats[state][county][scenarios[0]].keys()) if scenarios else None
 
+                if indicators != expected_indicators:
+                    raise Exception("statsForMap {}, {} is missing an expected indicator from {}"
+                        .format(state, county, expected_indicators))
+    
+    return set(scenario_names)
+
+
+def validate_scenario_names(geoid_scenarios: set, map_scenarios: set):
+    ''' Validate scenario names of geoid file match scenario names of statsForMap '''
+    if geoid_scenarios != map_scenarios:
+        raise Exception("County-level geoid scenarios {} do not match statsForMap scenarios {}"
+            .format(geoid_scenarios, map_scenarios))
+
+
+def validate_boundaries(boundaries: dict, expected_states: list, expected_counties: list):
+    ''' Validate countyBoundaries contain expected counties and states '''
+
+    states = sorted(list(boundaries.keys()))
+    for state in expected_states:
+        if state not in states:
+            raise Exception("County boundaries is missing state {}".format(state))
+        county_num = len(boundaries[state]['features'])
+        exp_counties = [x for x in expected_counties if x.startswith(state)]
+
+        for i in range(county_num):
+            county = boundaries[state]['features'][i]['properties']['geoid']
+            if county in exp_counties:
+                exp_counties.remove(county)
+        if exp_counties:
+            print("ERROR: countyBoundaries state {} is missing counties {}"
+                .format(state, exp_counties))
+
+
+def validate_actuals(actuals: list, expected_states: list, expected_counties: list):
+    ''' Validate expected number of county-level and state-level actuals files exists '''
+    counties = [x for x in actuals if len(x) == 5]
+    states = [x for x in actuals if len(x) == 2]
+
+    if len(states) < len(expected_states):
+        raise Exception("Expecting at least {} state-level actual.json files, not {}"
+            .format(len(expected_states), len(states)))
+
+    if len(counties) < len(expected_counties):
+        raise Exception("Expecting at least {} county-level actual.json files, not {}"
+            .format(len(expected_counties), len(counties)))
+
+
+def validate_reported_indicators(actual_obj: dict):
+    ''' 
+    Validate actual file includes expected reported indicators 
+    Confirm date format and value type are '%Y-%m%d' and int, respectively
+    '''
+    reported_indicators = {'incidC', 'incidD'}
+    if set(actual_obj.keys()) != reported_indicators:
+        raise Exception("Actual.json indicators {} do not match expected actual indicators {}"
+            .format(set(actual_obj.keys()), reported_indicators))
+
+    expected_format = '%Y-%m-%d'
+    for indicator in reported_indicators:
+        days = len(actual_obj[indicator])
+        for i in range(days):
+            date = actual_obj[indicator][i]['date']
+            value = actual_obj[indicator][i]['value']
+            try:
+                datetime.strptime(date, '%Y-%m-%d')
+            except:
+                raise Exception("Actual.json date format {} does not match expected format {}"
+                    .format(date, expected_format))
+            if not isinstance(value, int):
+                raise Exception("Actual.json value {} should be type int"
+                    .format(value))
 
 if __name__ == "__main__":
     ''' 
     config dir contains expected outcomes, counties, states 
     json_output dir contains model files to be validated
     '''
-    file_dir = 'json_output'
+    fdir = 'json_output'
 
-    with open('config/outcomes.json') as o, open('config/counties.json') as c, \
-        open('config/states.json') as s, open(file_dir + '/statsForMap.json') as sfm:
+    with open(fdir + '/outcomes.json') as o, open(fdir + '/counties.json') as c, \
+        open(fdir + '/states.json') as s, open(fdir + '/statsForMap.json') as m, \
+        open(fdir + '/countyBoundaries.json') as b:
         outcomes = json.load(o)
         expected_counties = sorted(list(json.load(c).keys()))
         expected_states = sorted(list(json.load(s).keys()))
+        stats_for_map, boundaries = json.load(m), json.load(b)
 
         ### STATS FOR MAP ###
-        stats_for_map = json.load(sfm)
-        validate_stats_for_map(stats_for_map, expected_states, expected_counties)
+        map_scenarios = validate_map_stats(stats_for_map, expected_states, expected_counties, outcomes)
+        validate_boundaries(boundaries, expected_states, expected_counties)
 
         ### COUNTY-LEVEL GEOID ###
-        counties = [f.strip('.json') for f in os.listdir(file_dir) if not f.startswith('.')]
-        for county in expected_counties:
-            print(county)
+        counties = [f.strip('.json') for f in os.listdir(fdir) if not f.startswith('.')]
+        for i, county in enumerate(expected_counties[3000:]):
+            if i%100==0: print(i, county)
             if county not in counties:
-                raise Exception("{} is missing county file {}.json".format(file_dir, county))
+                raise Exception("{} is missing county file {}.json".format(fdir, county))
 
-            with open('{}/{}.json'.format(file_dir, county)) as f:
+            with open('{}/{}.json'.format(fdir, county)) as f:
                 obj = json.load(f)
                 validate_geoid(obj, outcomes)
+
+                if county == '01001':
+                    validate_scenario_names(set(obj.keys()), map_scenarios)
 
         ### STATE-LEVEL GEOID ###
-        states = [f.strip('.json') for f in os.listdir(file_dir) if not f.startswith('.')]
+        states = [f.strip('.json') for f in os.listdir(fdir) if not f.startswith('.')]
         for state in expected_states:
             if state not in states:
-                raise Exception("{} is missing state file {}.json".format(file_dir, state))
+                raise Exception("{} is missing state file {}.json".format(fdir, state))
 
-            with open('{}/{}.json'.format(file_dir, county)) as f:
+            with open('{}/{}.json'.format(fdir, county)) as f:
                 obj = json.load(f)
                 validate_geoid(obj, outcomes)
 
-    print('Validation succeeded!')
+        ### ACTUALS ###
+        suffix = '_actuals.json'
+        actuals = [f.strip(suffix) for f in os.listdir(fdir) if not f.startswith('.') and f.endswith(suffix)]
+        validate_actuals(actuals, expected_states, expected_counties)
+
+        # just checking one file for reported indicators
+        with open(fdir + '/01001' + suffix) as a:
+            obj = json.load(a)
+            validate_reported_indicators(obj)
+
+    print('----***---- VALIDATION COMPLETE ----***----')
+
+# Missing geoids -->
+# "02158": "Kusilvak Census Area, AK",
+# "17157": "Randolph County, IL",
+# "46102": "Oglala Lakota County, SD",
+# "55033": "Dunn County, WI",
+# All of PR 72
