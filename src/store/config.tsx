5605194e800b34e7500d9bd34d3c1181cf2afd71
Lily Xu <lilyxu@alum.mit.edu>
2020-09-09 10:53:41 -0700

    cleanup

--- src/store/config.tsx
+++ src/store/config.tsx
@@ -4,27 +4,28 @@
 // s3 bucket location of 3147 county geoid jsons
 export const s3BucketUrl = process.env.NODE_ENV === 'development' ?
             'https://idd-dashboard-runs-staging.s3.amazonaws.com/json-files/'
+            // 'https://covid-scenario-dashboard.s3.amazonaws.com/json-files/'
             : 'json-files/'; // key prefix of relative bucket 
 
 // set file type to test to use_local = true and add update file_name value
 export const USE_LOCAL_GEOID = false
-export const LOCAL_GEOID = ''
+export const LOCAL_GEOID = '' //'06085.json'
 
-export const USE_LOCAL_ACTUALS = false
-export const LOCAL_ACTUALS = ''
+export const USE_LOCAL_ACTUALS = true
+export const LOCAL_ACTUALS = '06085_actuals.json'
 
 // static files
 export const CONFIGS = {
     'outcomes': {
-        'use_local': false,
-        'file_name': ''
+        'use_local': true,
+        'file_name': 'outcomes.json'
     },
     'statsForMap': {
-        'use_local': false,
-        'file_name': ''
+        'use_local': true,
+        'file_name': 'statsForMap.json'
     },
     'countyBoundaries': {
-        'use_local': false,
-        'file_name': ''
+        'use_local': true,
+        'file_name': 'countyBoundaries.json'
     }
 }
